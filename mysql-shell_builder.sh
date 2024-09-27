18a3fe78a3d90ab5fccc352a370376a216333280
Vadim Yalovets <vadim.yalovets@percona.com>
2024-09-27 13:54:58 +0300

    RM-1421 PS-8.4.2-2

--- mysql-shell_builder.sh
+++ mysql-shell_builder.sh
@@ -338,8 +338,8 @@ get_sources(){
         sed -i 's:execute_patchelf:# execute_patchelf:g' cmake/exeutils.cmake
     else
         sed -i 's:set(\"\${ARG_OUT_COMMAND}\" ${PATCHELF_EXECUTABLE}:#set(\"\${ARG_OUT_COMMAND}\" ${PATCHELF_EXECUTABLE}:g' cmake/exeutils.cmake
-        sed -i '/create a dependency/i \if(NOT TARGET \"${COPY_TARGET}\")' cmake/exeutils.cmake
-        sed -i '/APPEND COPIED_BINARIES/a endif()' cmake/exeutils.cmake
+        #sed -i '/create a dependency/i \if(NOT TARGET \"${COPY_TARGET}\")' cmake/exeutils.cmake
+        #sed -i '/APPEND COPIED_BINARIES/a endif()' cmake/exeutils.cmake
     fi
     sed -i 's:quilt:native:g' packaging/debian/source/format
     
